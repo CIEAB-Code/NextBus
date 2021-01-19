5388fa26e179b8c21ce9fb583abe447a8d7201e9
CIEAB-Code <60346590+CIEAB-Code@users.noreply.github.com>
2021-01-19 15:04:45 +0000

    automated testing
    Automated testing using unittest module.

---
+++ test_nextbus.py
@@ -0,0 +1,40 @@
+import unittest
+import nextbus
+import requests
+
+
+class TestNextBus(unittest.TestCase):
+    def test_homepage(self):
+        response = requests.get('http://localhost:5000')
+        self.assertEqual(response.status_code, 200)
+
+    def test_datapage(self):
+        response = requests.get('http://localhost:5000/data/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_bus_info(self):
+        data = nextbus.get_bus_info()
+        self.assertIsNotNone(data)
+
+    def test_query_data(self):
+        formatted_data = nextbus.query_data()
+        self.assertIsNotNone(formatted_data)
+
+    def test_filter_morning(self):
+        requests.get('http://localhost:5000')
+        formatted_data = nextbus.filter_morning(nextbus.query_data())
+        self.assertIsNotNone(formatted_data)
+
+    def test_filter_afternoon(self):
+        requests.get('http://localhost:5000')
+        formatted_data = nextbus.filter_afternoon(nextbus.query_data())
+        self.assertIsNotNone(formatted_data)
+
+    def test_filter_evening(self):
+        requests.get('http://localhost:5000')
+        formatted_data = nextbus.filter_evening(nextbus.query_data())
+        self.assertIsNotNone(formatted_data)
+
+
+if __name__ == '__main__':
+    unittest.main()
