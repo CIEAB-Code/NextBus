6bf5e787574b091ccf939acfd52250c884b68ff7
CIEAB-Code <60346590+CIEAB-Code@users.noreply.github.com>
2021-01-20 15:09:56 +0000

    change new file names

--- test_nextbus.py
+++ test_nextbus.py
@@ -1,12 +1,12 @@
 import unittest
-import nextbus
+import data_functions
 import requests
 
 
 class TestNextBus(unittest.TestCase):
     def setUp(self):
         requests.get('http://localhost:5000')
-        
+
     def test_homepage(self):
         response = requests.get('http://localhost:5000')
         self.assertEqual(response.status_code, 200)
@@ -16,23 +16,23 @@ class TestNextBus(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
 
     def test_get_bus_info(self):
-        data = nextbus.get_bus_info()
+        data = data_functions.get_bus_info()
         self.assertIsNotNone(data)
 
     def test_query_data(self):
-        formatted_data = nextbus.query_data()
+        formatted_data = data_functions.query_data()
         self.assertIsNotNone(formatted_data)
 
     def test_filter_morning(self):
-        formatted_data = nextbus.filter_morning(nextbus.query_data())
+        formatted_data = data_functions.filter_morning(data_functions.query_data())
         self.assertIsNotNone(formatted_data)
 
     def test_filter_afternoon(self):
-        formatted_data = nextbus.filter_afternoon(nextbus.query_data())
+        formatted_data = data_functions.filter_afternoon(data_functions.query_data())
         self.assertIsNotNone(formatted_data)
 
     def test_filter_evening(self):
-        formatted_data = nextbus.filter_evening(nextbus.query_data())
+        formatted_data = data_functions.filter_evening(data_functions.query_data())
         self.assertIsNotNone(formatted_data)
 
 
